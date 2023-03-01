784d312de735bf3e37c820359ae94695ae171688
Andi Martín <andi@MacBook-Pro-de-Andi.local>
2023-03-01 12:28:55 +0100

    Add new spring boot project

---
+++ asos/src/main/java/es/andim/asos/AsosApplication.java
@@ -0,0 +1,13 @@
+package es.andim.asos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsosApplication.class, args);
+	}
+
+}
---
+++ asos/src/test/java/es/andim/asos/AsosApplicationTests.java
@@ -0,0 +1,13 @@
+package es.andim.asos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
